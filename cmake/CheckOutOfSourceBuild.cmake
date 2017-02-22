29a6ca4d87b13768781c666f22722c3f7cab7647
teuthid <mariusz.przygodzki@gmail.com>
2017-02-22 21:06:27 +0100

    upload to master

--- cmake/CheckOutOfSourceBuild.cmake
+++ cmake/CheckOutOfSourceBuild.cmake
@@ -2,6 +2,5 @@ string(COMPARE EQUAL "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}" insource_)
 get_filename_component(PARENTDIR ${CMAKE_SOURCE_DIR} PATH)
 string(COMPARE EQUAL "${CMAKE_SOURCE_DIR}" "${PARENTDIR}" insourcesubdir_)
 if (insource_ OR insourcesubdir_)
-  message(FATAL_ERROR "Teuthid requires an out of source build!")
+  msg_error("Teuthid requires an out of source build!")
 endif()
-
--- cmake/TeuthidCheckBoost.cmake
+++ cmake/TeuthidCheckBoost.cmake
@@ -9,30 +9,24 @@ if (BUILD_TESTS)
   set(boost_optional_components_ unit_test_framework)
 endif(BUILD_TESTS)
 
-#set(Boost_REQUIRED_VERSION 1.61.0)
-find_package(Boost 1.61.0 QUIET REQUIRED 
+set(Boost_REQUIRED_VERSION 1.61.0)
+find_package(Boost ${Boost_REQUIRED_VERSION} QUIET REQUIRED 
   COMPONENTS ${boost_required_components_}
   OPTIONAL_COMPONENTS ${boost_optional_components_}) 
 
 if (NOT Boost_FOUND)
-  message(FATAL_ERROR 
-    "Cannot find Boost libraries: ${boost_required_components_}")
+  string(REPLACE ";" ", " components_ "${boost_required_components_}")
+  msg_error("Cannot find required Boost components: ${components_}!")
 else()
-  message(STATUS "Boost version:\
- ${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}")
+  msg_status("Boost version: " 
+    "${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}")
 endif()
 
-list(APPEND teuthid_link_libraries 
-  ${Boost_SYSTEM_LIBRARIES} 
-  ${Boost_MATH_C99_LIBRARIES} ${Boost_MATH_C99F_LIBRARIES}
-  ${Boost_MATH_C99L_LIBRARIES}
-  ${Boost_MATH_TR1_LIBRARIES} ${Boost_MATH_TR1F_LIBRARIES}
-  ${Boost_MATH_TR1L_LIBRARIES}
-  ${Boost_RANDOM_LIBRARIES})
-if (BUILD_WITH_OPENCL AND USE_BOOST_COMPUTE)
-  list(APPEND teuthid_link_libraries ${Boost_FILESYSTEM_LIBRARIES})
-endif()
+list(APPEND teuthid_link_libraries ${Boost_LIBRARIES})
 
 if (NOT Boost_UNIT_TEST_FRAMEWORK_FOUND AND BUILD_TESTS)
-  message(STATUS "Cannot find Boost.Test! Unit tests are disabled.")
+  msg_warning("Cannot find Boost.Test! Unit tests are disabled.")
 endif()
+
+unset(boost_required_components_)
+unset(boost_optional_components_)
