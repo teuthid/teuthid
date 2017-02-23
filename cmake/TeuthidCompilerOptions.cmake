56ce73563d5ca2f4d31ee4d1213f8dc93305fb77
teuthid <mariusz.przygodzki@gmail.com>
2017-02-23 19:10:15 +0100

    upload to master

--- cmake/TeuthidCompilerOptions.cmake
+++ cmake/TeuthidCompilerOptions.cmake
@@ -1,21 +1,12 @@
 include(CheckCXXCompilerFlag)
 
-set(list_avx_options_ avx avx2 __wrong__)
-set(list_sse_options_ sse sse2 sse3 ssse3 sse4a sse4.1 sse4.2 __wrong__)
+set(list_AVX_options_ avx avx2 __wrong__)
+set(list_SSE_options_ sse sse2 sse3 ssse3 sse4a sse4.1 sse4.2 __wrong__)
 
 # GCC depresses SSEx instructions when -mavx is used. Instead, it generates new # AVX instructions or AVX equivalence for all SSEx instructions when needed. 
 # To generate SSE/SSE2 instructions automatically from floating-point code
 # (as opposed to 387 instructions), see -mfpmath=sse.
 # These options are defined for the i386 and x86-64 family of computers.
-
-function(teuthid_add_compiler_option option_ result_)
-  check_cxx_compiler_flag("-m${option_}" moption_${option_})
-  if (moption_${option_})
-    set(result_ "-m${option_}" PARENT_SCOPE)
-  else()
-    set(result_ "" PARENT_SCOPE)
-  endif()
-endfunction(teuthid_add_compiler_option)
 
 if (CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
   set(teuthid_compiler_options ${CMAKE_CXX_FLAGS_RELWITHDEBINFO})
@@ -30,27 +21,45 @@ else()
 endif()
 set(teuthid_tests_compiler_options ${teuthid_compiler_options})
 
-set(found_AVX OFF)
-set(found_SSE OFF)
+macro(teuthid_add_compiler_option option_ result_)
+  set(teuthid_message_switch OFF)
+  check_cxx_compiler_flag("-m${option_}" moption_${option_})
+  set(teuthid_message_switch ON)
+  if (moption_${option_})
+    set(result_ "-m${option_}")
+  endif()
+endmacro(teuthid_add_compiler_option)
 
-if (ENABLE_AVX_SSE)
-  message(STATUS "Check for working compiler options (AVX/SSE)")
-  #set(teuthid_message_switch OFF)
-  foreach(option_ ${list_avx_options_})
-    teuthid_add_compiler_option("${option_}" result_)
+macro(teuthid_add_compiler_options options_)
+  foreach(ext_ ${list_${options_}_options_})
+    teuthid_add_compiler_option("${ext_}" result_)
     if (result_)
-      set(teuthid_compiler_options "${teuthid_compiler_options} ${result_}")
-      set(found_AVX_ ON)
-    endif() 
+      set(AVX_SSE_compiler_options "${AVX_SSE_compiler_options} ${result_}")
+      set(found_extensions_ ${options_})
+    endif()
     unset(result_)
   endforeach()
-  if (found_AVX_)
-    set(teuthid_compiler_options "${teuthid_compiler_options} -mfpmath=sse")
-  endif()
-  #set(teuthid_message_switch ON)
-  message(STATUS "Check for working compiler options (AVX/SSE) - done") 
-endif(ENABLE_AVX_SSE)
+endmacro()
 
-unset(found_AVX)
-unset(found_SSE)
+set(found_extensions_ NONE)
+set(AVX_SSE_compiler_options)
+set(AVX_SSE_compiler_options_for_tests)
+
+if (ENABLE_AVX_SSE OR ENABLE_AVX_SSE_FOR_TESTS)
+  message(STATUS "Check for working compiler options (AVX/SSE)")
+  teuthid_add_compiler_options(AVX)
+  if (${found_extensions_} MATCHES NONE)
+    teuthid_add_compiler_options(SSE)
+  endif()
+  if (NOT (${found_extensions_} MATCHES NONE))
+    set(AVX_SSE_compiler_options "${AVX_SSE_compiler_options} -mfpmath=sse")
+    msg_status("Check for working compiler options (AVX/SSE) - " 
+      "${found_extensions_}")
+  else()
+    msg_status("Check for working compiler options (AVX/SSE) - " 
+      "${Yellow}not found${ColorReset}")
+  endif()
+endif()
+
+unset(found_extensions_)
 set(CMAKE_VERBOSE_MAKEFILE ON) # to comment out
