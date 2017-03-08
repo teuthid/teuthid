fe3aad8165607fc315fcf02f5af30021fcc91220
teuthid <mariusz.przygodzki@gmail.com>
2017-03-08 21:35:41 +0100

    upload to master

--- src/cl_platform_info.cpp
+++ src/cl_platform_info.cpp
@@ -43,6 +43,19 @@ const opencl_platforms_t &platform_info::platforms(bool force_detection) {
   return platform_info::platforms_;
 }
 
+bool __teuthid_cl_platform_param(std::string &param, cl_platform_id platform,
+                                 cl_platform_info param_name) {
+  cl_int __result;
+  char __data[1024];
+  std::size_t __retsize;
+  __result = clGetPlatformInfo(platform, param_name, sizeof(__data), __data,
+                               &__retsize);
+  if ((__result != CL_SUCCESS) || (__retsize == sizeof(__data)))
+    return false;
+  param = std::string(__data);
+  return true;
+}
+
 void platform_info::detect_platforms_and_devices_() {
   platform_info::platforms_.clear();
 #if defined(TEUTHID_WITH_OPENCL)
@@ -69,25 +82,22 @@ void platform_info::detect_platforms_and_devices_() {
     assert(__platforms[i]);
     platforms_.push_back(platform_info());
     platforms_[i].id_ = __platforms[i];
-    __result = clGetPlatformInfo(__platforms[i], CL_PLATFORM_PROFILE,
-                                 sizeof(__data), __data, &__retsize);
-    if ((__result != CL_SUCCESS) || (__retsize == sizeof(__data))) {
+
+    if (!__teuthid_cl_platform_param(__str, __platforms[i],
+                                     CL_PLATFORM_PROFILE)) {
       platforms_[i].profile_ = UNKNOWN_PROFILE;
       continue; // unable to get platform's profile
     }
-    __str = std::string(__data);
     if (__str == std::string("FULL_PROFILE"))
       platforms_[i].profile_ = FULL_PROFILE;
     else if (__str == std::string("EMBEDDED_PROFILE"))
       platforms_[i].profile_ = EMBEDDED_PROFILE;
     else
       platforms_[i].profile_ = UNKNOWN_PROFILE;
-    __result = clGetPlatformInfo(__platforms[i], CL_PLATFORM_VERSION,
-                                 sizeof(__data), __data, &__retsize);
-    if ((__result != CL_SUCCESS) || (__retsize == sizeof(__data)))
+    if (!__teuthid_cl_platform_param(__str, __platforms[i],
+                                     CL_PLATFORM_VERSION))
       assert(false);
     else {
-      __str = std::string(__data);
       __str = __str.substr(7);
       assert(!__str.empty());
       platforms_[i].version_ = __str;
@@ -100,37 +110,24 @@ void platform_info::detect_platforms_and_devices_() {
           std::stoi(__str.substr(__dot + 1, __space));
       platforms_[i].spec_version_ = __str.substr(__space + 1);
     }
-    __result = clGetPlatformInfo(__platforms[i], CL_PLATFORM_NAME,
-                                 sizeof(__data), __data, &__retsize);
-    if ((__result != CL_SUCCESS) || (__retsize == sizeof(__data)))
+    if (!__teuthid_cl_platform_param(platforms_[i].name_, __platforms[i],
+                                     CL_PLATFORM_NAME))
       assert(false);
-    else
-      platforms_[i].name_ = std::string(__data);
-    __result = clGetPlatformInfo(__platforms[i], CL_PLATFORM_VENDOR,
-                                 sizeof(__data), __data, &__retsize);
-    if ((__result != CL_SUCCESS) || (__retsize == sizeof(__data)))
+    if (!__teuthid_cl_platform_param(platforms_[i].vendor_, __platforms[i],
+                                     CL_PLATFORM_VENDOR))
       assert(false);
-    else
-      platforms_[i].vendor_ = std::string(__data);
-    __result = clGetPlatformInfo(__platforms[i], CL_PLATFORM_EXTENSIONS,
-                                 sizeof(__data), __data, &__retsize);
-    if ((__result != CL_SUCCESS) || (__retsize == sizeof(__data)))
-      ; // no vendor ?!
-    else
-      platforms_[i].extensions_ = std::string(__data);
-    __result =
-        clGetPlatformInfo(__platforms[i], CL_PLATFORM_HOST_TIMER_RESOLUTION,
-                          sizeof(__data), __data, &__retsize);
-    if ((__result != CL_SUCCESS) || (__retsize == sizeof(__data)))
+    if (!__teuthid_cl_platform_param(platforms_[i].extensions_, __platforms[i],
+                                     CL_PLATFORM_EXTENSIONS))
+      assert(false);
+    if (!__teuthid_cl_platform_param(__str, __platforms[i],
+                                     CL_PLATFORM_HOST_TIMER_RESOLUTION))
       ; // not supported
     else
-      platforms_[i].host_timer_resolution_ = std::stoull(__data);
-    __result = clGetPlatformInfo(__platforms[i], CL_PLATFORM_ICD_SUFFIX_KHR,
-                                 sizeof(__data), __data, &__retsize);
-    if ((__result != CL_SUCCESS) || (__retsize == sizeof(__data)))
+      platforms_[i].host_timer_resolution_ = std::stoull(__str);
+    if (!__teuthid_cl_platform_param(platforms_[i].icd_suffix_khr_,
+                                     __platforms[i],
+                                     CL_PLATFORM_ICD_SUFFIX_KHR))
       ; // not supported
-    else
-      platforms_[i].icd_suffix_khr_ = std::string(__data);
 
     // devices queries
     // TO DO
