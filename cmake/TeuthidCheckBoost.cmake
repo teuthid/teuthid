bbc24314b32560c8129bdd4b708a9d9cc13c6cfd
teuthid <mariusz.przygodzki@gmail.com>
2017-05-19 18:22:02 +0200

    upload to master

--- cmake/TeuthidCheckBoost.cmake
+++ cmake/TeuthidCheckBoost.cmake
@@ -1,29 +1,18 @@
-if (BUILD_WITH_OPENCL)
-  list(APPEND Boost_REQUIRED_COMPONENTS filesystem)
-endif()
-
-if (BUILD_TESTS)
-  set(boost_optional_components_ unit_test_framework)
-endif(BUILD_TESTS)
 
 set(Boost_USE_MULTITHREADED ON)
-find_package(Boost ${Boost_REQUIRED_VERSION} REQUIRED 
-  COMPONENTS ${Boost_REQUIRED_COMPONENTS}
-  OPTIONAL_COMPONENTS ${boost_optional_components_}) 
+find_package(Boost ${Boost_REQUIRED_VERSION} QUIET REQUIRED 
+  COMPONENTS ${Boost_REQUIRED_COMPONENTS}) 
 
 if (NOT Boost_FOUND)
   string(REPLACE ";" ", " components_ "${Boost_REQUIRED_COMPONENTS}")
-  msg_error("Cannot find required Boost components: ${components_}!")
+  msg_warning("Cannot find Boost components: ${components_}!")
+  msg_warning("Unit tests are disabled.")
+  set(BUILD_TESTS OFF)
 else()
   msg_status("Boost version: " 
     "${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}\
  (required ${Boost_REQUIRED_VERSION})")
-endif()
-
-list(APPEND teuthid_link_libraries ${Boost_LIBRARIES})
-list_prepend(teuthid_INCLUDE_PATH ${Boost_INCLUDE_DIRS})
-list(REMOVE_DUPLICATES teuthid_INCLUDE_PATH)
-
-if (NOT Boost_UNIT_TEST_FRAMEWORK_FOUND AND BUILD_TESTS)
-  msg_warning("Cannot find Boost.Test! Unit tests are disabled.")
+  list(APPEND teuthid_tests_link_libraries ${Boost_LIBRARIES})
+  list_prepend(teuthid_tests_INCLUDE_PATH ${Boost_INCLUDE_DIRS})
+  list(REMOVE_DUPLICATES teuthid_tests_INCLUDE_PATH)
 endif()
