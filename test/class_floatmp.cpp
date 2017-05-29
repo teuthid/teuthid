61c21f1588f7a7e5a1dd1bf91cc34bcb129fa20b
teuthid <mariusz.przygodzki@gmail.com>
2017-05-29 18:40:25 +0200

    upload to master

--- test/class_floatmp.cpp
+++ test/class_floatmp.cpp
@@ -38,14 +38,24 @@ BOOST_AUTO_TEST_CASE(class_teuthid_floatmp) {
 
   system::format_float_output();
   BOOST_TEST((__x1 == __x2), "operator==");
+  BOOST_TEST(!(__x1 < __x2), "operator<");
+  BOOST_TEST(!(__x1 > __x2), "operator>");
+  BOOST_TEST((__x1 <= __x2), "operator<=");
+  BOOST_TEST((__x1 >= __x2), "operator>=");
   BOOST_TEST((__x1 == __x3), "operator==");
   __x1.assign(-13);
   BOOST_TEST((__x1 != __x3), "operator!=");
+  BOOST_TEST((__x1 < __x3), "operator<");
+  BOOST_TEST((__x1 <= __x3), "operator<=");
+  BOOST_TEST(!(__x1 > __x3), "operator>");
+  BOOST_TEST(!(__x1 >= __x3), "operator>=");
   __x1.assign(12345);
   __x2.assign(12345);
   BOOST_TEST((__x1 == __x2), "operator==");
   __x3.assign(-12345);
   BOOST_TEST((__x1 != __x3), "operator!=");
+  BOOST_TEST((__x1 > __x3), "operator>");
+  BOOST_TEST((__x3 < __x1), "operator<");
   __x1.assign((float)1.234);
   __x3.assign(__x1);
   BOOST_TEST((__x1 == __x3), "operator==");
@@ -62,11 +72,13 @@ BOOST_AUTO_TEST_CASE(class_teuthid_floatmp) {
   BOOST_TEST(!system::to_string(__x1).empty());
   BOOST_TEST(!system::to_string(__x3).empty());
   BOOST_TEST((__x1 == __x3), "operator==");
+  BOOST_TEST(!(__x1 < __x3), "operator<");
   __x1.assign(static_cast<uint128_t>(UINT64_MAX) * 10);
   __x3.assign(__x1);
   BOOST_TEST(!system::to_string(__x1).empty());
   BOOST_TEST(!system::to_string(__x3).empty());
   BOOST_TEST((__x1 == __x3), "operator==");
+  BOOST_TEST(!(__x1 < __x3), "operator<");
 #endif // TEUTHID_HAVE_INT_128
   //
 }
