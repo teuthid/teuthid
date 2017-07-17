5d72b76c83798effa21a4a6b9c121b2afbda2022
teuthid <mariusz.przygodzki@gmail.com>
2017-07-17 20:32:14 +0200

    upload to master

--- src/clb/cl_bindings.hpp
+++ src/clb/cl_bindings.hpp
@@ -1,19 +1,20 @@
 /*
   This file is part of the Teuthid project.
-  Copyright (C) 2016-2017 Mariusz Przygodzki (mariusz.przygodzki@gmail.com)
+  Copyright (c) 2016-2017 Mariusz Przygodzki (mariusz.przygodzki@gmail.com)
 
-    This library is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  The Teuthid is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or (at your
+  option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  The Teuthid is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+  for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with the Teuthid.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU Lesser General Public License
+  along with the Teuthid; see the file LICENSE.LGPLv3.  If not, see
+  <http://www.gnu.org/licenses/>.
 */
 
 // This is the internal header file.
